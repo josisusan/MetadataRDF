3575c9ddf53934002582b6330dbe2f969adfa622
Susan Joshi <josisusan@gmail.com>
2012-05-30 21:24:27 +0545

    Extract features from image like iso, shutter speed

---
+++ src/metadatardf/ImageFeatures.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metadatardf;
+
+/**
+ *
+ * @author susan
+ */
+public class ImageFeatures {
+    
+}
